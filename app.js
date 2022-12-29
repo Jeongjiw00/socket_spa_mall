34743bca9b916ba5a55ee7b42b4d0b0031e03e35
Jeongjiw00 <wldn0229@naver.com>
2022-12-30 01:27:20 +0900

    jest supertest

--- app.js
+++ app.js
@@ -214,4 +214,8 @@ router.post("/goods", async (req, res) => {
 
 app.use("/api", express.urlencoded({ extended: false }), router);
 
+http.listen(8080, () => {
+  console.log('서버가 요청을 받을 준비가 됐어요');
+})
+
 module.exports = http;
--- middlewares/auth-middleware.js
+++ middlewares/auth-middleware.js
@@ -1,26 +1,51 @@
-const jwt = require("jsonwebtoken");
+// middlewares/auth-middleware.spec.js
+
+const authMiddleware = require("./auth-middleware");
+
+jest.mock("../models");
+
 const { User } = require("../models");
 
-module.exports = (req, res, next) => {
-  const { authorization } = req.headers;
-  const [authType, authToken] = (authorization || "").split(" ");
+test("정상적인 토큰을 넣은 경우 User.findByPk가 실행된다.", () => {
+  User.findByPk = jest.fn();
 
-  if (!authToken || authType !== "Bearer") {
-    res.status(401).send({
-      errorMessage: "로그인 후 이용 가능한 기능입니다.",
-    });
-    return;
-  }
+  authMiddleware(
+    {
+      headers: {
+        authorization:
+          "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjk5LCJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.27uuQ34QFqdzibQrQSJ0OKyI4wqIXz6wVvJ1uSlcESQ",
+      },
+    },
+    {
+      status: () => ({
+        send: () => {},
+      }),
+      locals: {},
+    }
+  );
 
-  try {
-    const { userId } = jwt.verify(authToken, "customized-secret-key");
-    User.findByPk(userId).then((user) => {
-      res.locals.user = user;
-      next();
-    });
-  } catch (err) {
-    res.status(401).send({
-      errorMessage: "로그인 후 이용 가능한 기능입니다.",
-    });
-  }
-};
+  expect(User.findByPk).toHaveBeenCalledTimes(1);
+  expect(User.findByPk).toHaveBeenCalledWith(99);
+});
+
+test("변조된 토큰으로 요청한 경우 로그인 후 사용하세요 라는 에러 메세지가 뜬다.", () => {
+  const mockedSend = jest.fn();
+
+  authMiddleware(
+    {
+      headers: {
+        authorization: "Bearer ",
+      },
+    },
+    {
+      status: () => ({
+        send: mockedSend,
+      }),
+      locals: {},
+    }
+  );
+
+  expect(mockedSend).toHaveBeenCalledWith({
+    errorMessage: "로그인 후 이용 가능한 기능입니다.",
+  });
+});
